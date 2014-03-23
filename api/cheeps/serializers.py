740a22c2be462d98179560b454bf54b1fb75ef7d
Harish Narayanan <hnarayanan@gmail.com>
2014-03-23 18:22:34 +0100

    Tentatively split userserializer into two

--- api/cheeps/serializers.py
+++ api/cheeps/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 
-from users.serializers import UserSerializer
+from users.serializers import CheepUserSerializer
 from .models import Cheep
 
 
 class CheepSerializer(serializers.HyperlinkedModelSerializer):
 
-    author = UserSerializer()
+    author = CheepUserSerializer(read_only=True)
 
     class Meta:
         model = Cheep
--- api/users/serializers.py
+++ api/users/serializers.py
@@ -3,14 +3,21 @@ from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class CheepUserSerializer(serializers.HyperlinkedModelSerializer):
 
-    thumbnail_url = serializers.CharField(source='thumbnail_url')
+    thumbnail_url = serializers.CharField(source='thumbnail_url', read_only=True)
+
+    class Meta:
+        model = User
+        cheeps = serializers.RelatedField(many=True)
+        fields = ('name', 'handle', 'thumbnail_url')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = User
         cheeps = serializers.RelatedField(many=True)
         is_following = serializers.RelatedField(many=True)
         has_followers = serializers.RelatedField(many=True)
-        fields = ('name', 'handle', 'thumbnail_url')
-#        fields = ('url', 'name', 'handle', 'thumbnail', 'cheeps', 'is_following', 'has_followers')
+        fields = ('url', 'name', 'handle', 'thumbnail', 'cheeps', 'is_following', 'has_followers')
