c7a8f06123391b22f4414d77da29400e42eba125
Harish Narayanan <hnarayanan@gmail.com>
2014-03-26 22:05:26 +0100

    Remove resource use for now

--- app/scripts/controllers/user-detail.js
+++ app/scripts/controllers/user-detail.js
@@ -1,6 +1,13 @@
 'use strict';
 
 angular.module('cheeperApp')
-  .controller('UserDetailCtrl', function ($scope, $routeParams, User) {
-    $scope.pde = User.get({userId: $routeParams.userId});
+  .controller('UserDetailCtrl', function ($scope, $http, $routeParams) {
+    $http({method: 'GET', url: 'http://127.0.0.1:8000/users/2/cheeps/'}).
+      success(function(data, status, headers, config) {
+        $scope.stream = data;
+      }).
+      error(function(data, status, headers, config) {
+    });
+//    $scope.user = User.get({userId: $routeParams.userId});
+//    $scope.stream = user.cheeps;
   });
--- app/scripts/services/api.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('cheeperApp')
-  .factory('User', function ($resource) {
-      return $resource('http://localhost:8000/users/:userId', {}, {
-        query: { params: { userId:'' } }
-      });
-    });
