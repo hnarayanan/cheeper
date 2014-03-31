795efe8418279940a343c51ade0c48d01c486d40
Harish Narayanan <hnarayanan@gmail.com>
2014-03-31 18:23:44 +0100

    Wire up fake new cheep function

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,4 +8,20 @@ angular.module('cheeperApp')
       }).
       error(function(data, status, headers, config) {
     });
+    $scope.add = function() {
+       $scope.stream.push({
+        "id": 8,
+        "url": "http://localhost:8000/cheeps/8/",
+        "created": "2014-03-25T10:39:35.211Z",
+        "modified": "2014-03-25T10:40:42.524Z",
+        "author": {
+            "id": 3,
+            "url": "http://localhost:8000/users/3/",
+            "name": "Evil Bob",
+            "handle": "bob",
+            "thumbnail_url": "http://localhost:8000/media/thumbnails/83TUFUOY_bigger.jpeg"
+        },
+        "content": $scope.cheep.content
+       });
+    };
 });
